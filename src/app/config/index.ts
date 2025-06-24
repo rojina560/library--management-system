fcf16f0a9ec2af21d3149883b0fafc8535031f7e
Your Name <you@example.com>
2025-06-24 08:24:43 +0600

    updated

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -3,7 +3,7 @@ import path from 'path'
 dotenv.config({path:path.join(process.cwd(),'.env')})
 
 export default{
-    node_env:process.env.NODE_ENV,
+    node_env:process.env.NODE_ENV || 5000,
     port:process.env.PORT,
     database_url:process.env.DATABASE_URL
 }
